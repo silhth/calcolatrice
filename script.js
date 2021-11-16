508a5cbfc3e6e2e9ea8420ead33d1843143df98d
silvia.hth <silvia.hth@gmail.com>
2021-11-16 16:51:53 +0100

    calcolatrice 16Nov

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-
-function saveCredentials(operation) {
-    window.localStorage.setItem("operation", operation);
-}
-
-function loadCredentials(key) {
-    return window.localStorage.getItem(key);
-}
-
-function calculator(numbers) {
-    try {
-
-        const sum = () => numbers.reduce((acc, num) => acc + num);
-        const sub = () => numbers.reduce((acc, num) => acc - num);
-        const mul = () => numbers.reduce((acc, num) => acc * num);
-        const div = () => numbers.reduce((acc, num) => acc / num);
-        const pow = () => numbers.reduce((acc, num) => Math.pow(acc, num));
-        const powEach = () => numbers.map(num => num * num);
-        const sqrt = () => numbers.map(num => Math.sqrt(num));
-
-        if (numbers.length === 1 && operator[0] !== '**each' && operator[0] !== 'sqrt') throw "errore: Hai passato un solo valore"
-        if (numbers.length === 0) throw "Nessun parametro passato alla funzione"
+function calculator(numOne, numTwo) {
+           
+        const sum = () => numOne + numTwo;
+        const mul = () => numOne * numTwo;
+        const sub = () => numOne - numTwo;
+        const div = () => numOne / numTwo;
+        const pow = () => Math.pow(numOne,numTwo);
+        const sqrt = () => Math.sqrt(numOne);
 
         return {
             sum: sum(),
@@ -27,145 +13,68 @@ function calculator(numbers) {
             mul: mul(),
             div: div(),
             pow: pow(),
-            powEach: powEach(),
             sqrt: sqrt()
-        };
-    } catch (err) {
-
-        switch (err) {
-            case 'errore: Hai passato un solo valore':
-                console.warn('servono almeno due valori per far funzionare la calcolatrice ')
-                break;
-
-            case 'Nessun parametro passato alla funzione':
-                console.error('devi inserire i numeri per il calcolo')
-                break;
-        }
-
+        } 
     }
 
-}
 
-function addNum (numberBtn, arr) {
+function addNum (numberBtn, arr, log) {
     
     numberBtn.addEventListener('click', (e) => {
         
-        arr.push (parseInt(numberBtn.textContent));
-        console.log(arr);
-      
-}
-)
-}
+        arr.push (numberBtn.textContent);
+        console.log(arr)
+        log.innerHTML = numberBtn.textContent;      
+})}
 
 
-function addOper (operators) {
+function addOper (operators, log) {
     operators.addEventListener('click', (e) => {
-    arrNumbers.push(parseInt(arrNums.join('')));
-    console.log(arrNumbers)
-    arrNums.splice(0,arrNums.length)
-    operator.push (operators.textContent)
-    
-}
-
-)
-
-}
-
-function addOper1 (operators) {
-    operators.addEventListener('click', (e) => {
-    arrNumbers.push(parseInt(arrNums.join('')));
-    console.log(arrNumbers)
-    arrNums.splice(0,arrNums.length)
-    
-}
-
-)
-
-}
+        if (arrNums.length > 0) {arrNumbers.push(arrNums.join(''))};
+        console.log(arrNumbers)
+        arrNums.splice(0,arrNums.length)
+        operator.push (operators.textContent)
+        log.innerHTML = operators.textContent; 
+})}
 
 
 
-const inputNumbers = document.querySelector('#nums');
-const inputOperator = document.querySelector('#oper');
-const btnCalc = document.querySelector('#btnCalc');
 const recap = document.querySelector('#recap');
 const reload =document.querySelector('#reload')
-const log = document.querySelector('.operation');
+const log = document.querySelector('.result');
+const logOperation = document.querySelector('.operation');
 
-// const nums = document.querySelectorAll('.num')
-// console.log(nums)
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-const num3 = document.querySelector('#num3');
-const num4 = document.querySelector('#num4');
-const num5 = document.querySelector('#num5');
-const num6 = document.querySelector('#num6');
-const num7 = document.querySelector('#num7');
-const num8 = document.querySelector('#num8');
-const num9 = document.querySelector('#num9');
-const num0 = document.querySelector('#num0');
-// console.log(num1)
-
-const sum = document.querySelector('#sum');
-const sub = document.querySelector('#sub');
-const mul = document.querySelector('#mul');
-const divi = document.querySelector('#div');
-const pow = document.querySelector('#pow');
-const powEach = document.querySelector('#powEach');
-const sqtr = document.querySelector('#sqtr');
-
-let arrNums =[]
-
-const arrNumbers = []
-
-const operator= []
-
-addNum (num1, arrNums);
-addNum (num2, arrNums);
-addNum (num3, arrNums);
-addNum (num4, arrNums);
-addNum (num5, arrNums);
-addNum (num6, arrNums);
-addNum (num7, arrNums);
-addNum (num8, arrNums);
-addNum (num9, arrNums);
-addNum (num0, arrNums);
+const nums = [...document.querySelectorAll('.num')]
+const operators = [...document.querySelectorAll('.operator')]
 
 
-addOper (sum);
-addOper (sub);
-addOper (mul);
-addOper (divi);
-addOper (pow);
-addOper (powEach);
-addOper (sqrt);
-addOper (recap);
+let arrNums =[];
+const arrNumbers = [];
+const operator= [];
+
+nums.forEach((item)=> addNum(item, arrNums, log));
 
 
+operators.forEach ((item)=> {addOper(item, log)
+        item.addEventListener("click", () => {
+            const arrNums = arrNumbers.map(num => parseFloat(num))
+            let resutls = calculator(...arrNums)
 
+            logOperation.innerHTML = arrNumbers.join (operator[operator.length -2])
+            
+            if (operator[operator.length -2] === '+') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.sum);log.innerHTML = resutls.sum }
+            if (operator[operator.length -2] === '-') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.sub); log.innerHTML = resutls.sub }
+            if (operator[operator.length -2] === '*') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.mul); log.innerHTML =resutls.mul}
+            if (operator[operator.length -2] === '/') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.div); log.innerHTML =resutls.div }
+            if (operator[operator.length -2] === '**') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.pow); log.innerHTML = resutls.pow }
+            if (operator[operator.length -2] === 'sqrt') {arrNumbers.splice(0,arrNumbers.length); arrNumbers.push(resutls.sqrt); log.innerHTML = resutls.sqrt}
 
-btnCalc.addEventListener("click", (eve) => {
-    console.log (arrNumbers)
-    console.log (operator)
-    loadCredentials(saveCredentials(arrNumbers.join(operator[0])));
-    log.innerHTML = (arrNumbers.join(operator[0]));
-})
+        });
+        
+        
+        reload.addEventListener('click', function(){location.reload()})
+});
 
-
-recap.addEventListener("click", (eve) => {
-    const resutls = calculator(arrNumbers)
-    console.log(resutls)
-    if (operator[0] === '+') { log.innerHTML = resutls.sum }
-    if (operator[0] === '-') {log.innerHTML = resutls.sub }
-    if (operator[0] === '*') {log.innerHTML =resutls.mul}
-    if (operator[0] === '/') { log.innerHTML =resutls.div }
-    if (operator[0] === '**') { log.innerHTML = resutls.pow }
-    if (operator[0] === '**each') { log.innerHTML = resutls.powEach }
-    if (operator[0] === 'sqrt') { log.innerHTML = resutls.sqrt}
-
-})
-
-reload.addEventListener('click', function(){location.reload()})
 
 
 
